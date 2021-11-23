29417a3abf1591bd389c0c233f88ac133429b7b3
KimByungHun <k01087831451@gmail.com>
2021-11-23 13:39:58 +0900

    텍스트 에니메이션

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ window.onload=function(){
         speed:0.2
     });
 };
+
+const btn = document.querySelector(".ripple");
+
+btn.onmousemove = function(e){
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty('--x',x + 'px');
+    btn.style.setProperty('--y',y + 'px');
+}
