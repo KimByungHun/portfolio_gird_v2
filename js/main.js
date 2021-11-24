a91c6bb61eeabcccefd5db567a58ab6a8d994edc
KimByungHun <k01087831451@gmail.com>
2021-11-24 23:33:43 +0900

    box Rotate Text (반응형 고려 전)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,11 @@ btn.onmousemove = function(e){
     btn.style.setProperty('--x',x + 'px');
     btn.style.setProperty('--y',y + 'px');
 }
+
+//텍스트 서클
+
+const text = document.querySelector(".text p");
+text.innerHTML = text.innerText.split("").map(
+    (char, i)=>
+    `<span class='circle_txt' style="transform:rotate(${i * 13}deg")>${char}</span>`
+).join("")
