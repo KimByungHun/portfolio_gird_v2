29d69ae01a479c3643934d386da1daddc2abb541
KimByungHun <k01087831451@gmail.com>
2021-11-25 02:59:06 +0900

    text glowing

--- js/main.js
+++ js/main.js
@@ -17,8 +17,34 @@ btn.onmousemove = function(e){
 
 //텍스트 서클
 
-const text = document.querySelector(".text p");
-text.innerHTML = text.innerText.split("").map(
-    (char, i)=>
-    `<span class='circle_txt' style="transform:rotate(${i * 13}deg")>${char}</span>`
-).join("")
+// const text = document.querySelector(".text p");
+// const circle= document.querySelector(".circle");
+// const circleTxt = document.querySelector(".circle_txt");
+
+// text.innerHTML = text.innerText.split("").map(
+//     (char, i)=>
+//     `<span class='circle_txt' style="transform:rotate(${i * 13}deg")>${char}</span>`
+// ).join("")
+
+// var circleWidth = getComputedStyle(circle).width;
+// var circleHeight = getComputedStyle(circle).height;
+// var origin = getComputedStyle(circleTxt).transform-origin;
+
+// console.log(origin)
+
+// if(circleWidth>circleHeight){
+//     circleWidth = circleHeight;
+//     circle.style.setProperty('height',circleWidth);
+// }else{
+//     // circleHeight = circleWidth;
+//     circle.style.setProperty('height',circleWidth);
+// }
+
+// Origin.style.setProperty('transform-origin','0 0px');
+
+
+
+
+// var circleHeight = getComputedStyle(circle).height;
+// console.log(circleWidth);
+// console.log(circleHeight);
