34c35205ea57bebbfb9c3334c6fb39e29268fea6
KimByungHun <k01087831451@gmail.com>
2021-12-07 22:15:16 +0900

    인스턴스 옵션값 전달 오류

--- js/form.js
+++ js/form.js
@@ -1,42 +1,43 @@
-// $("input[type=submit]").on("click", function(e){
-    
-// })
 
-// function isTxt(name, len){
-  
-// }
+function MyForm(selector, options){
 
-function MyForm(){
-    this.init();
-    this.bindingEvent();
+    if(!selector){
+       console.error("form 선택자는 필수 입력사항입니다"); 
+       return;
+    }
+    this.init(selector); 
+    this.bindingEvent(options); 
+ }
+
+ MyForm.prototype.init = function(selector){
+    this.form = $(selector); 
+    this.btnSubmit = this.form.find("input[type='submit']"); 
+ }
+
+ MyForm.prototype.bindingEvent = function(options){
+    options.forEach(function(opt){
+        this.btnSubmit.on("click", function(e){
+            if(opt.type==="text"){
+                if(!this.isTxt(opt.name, opt.len)) e.preventDefault(); 
+             }
+        }.bind(this));
+    }.bind(this));
 }
-
-MyForm.prototype.init = function(){
-    this.form = document.querySelector("#myForm");
-    this.btnSubmit = 
-    this.form.querySelector("input[type=submit]");
-}
-
-MyForm.prototype.bindingEvent = function(){
-    this.btnSubmit.on("click", function(e){
-        if(!isTxt('message',1)) e.preventDefault();
-    })
-}
-
 
 MyForm.prototype.isTxt = function(name, len){
-    if(len === undefined) len = 1;
-
-    let txt = $("[name = "+name+"]").val();
-
+    if(len === undefined ) len = 1; 
+    
+    let txt = $("[name = "+name+"]").val(); 
+     
     if(txt.length >= len){
-        $("[name = "+name+"]").parent().find("p").remove();
-        return true;
+       $("[name = "+name+"]").parent().find("p").remove(); 
+       return true; 
     }else{
-        $("[name = "+name+"]").parent().find("p").remove();
-        $("[name = "+name+"]").parent().find("p").append(
-            "<p>메세지를 "+len+"자 이상 입력하세요</p>"
+        $("[name = "+name+"]").parent().find("p").remove(); 
+        $("[name = "+name+"]").parent().append(
+            "<p>아이디를 "+len+"자 이상 입력하세요</p>"
         )
-        return false;
+       return false; 
     }
 }
+
